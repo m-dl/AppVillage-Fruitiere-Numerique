5e4996e48cbe72a4d9d71c4a8ca591dbc9aebaab
m-dl <fullcontactsoldier@gmail.com>
2016-04-09 16:14:54 +0200

    Intégration DriveAPI Android et mise à jour auto des médias TODO: modifie onResume de BaseActivity, sinon relance le dl si app est pausée

---
+++ driveAPI/src/main/java/com/google/android/gms/drive/HomeActivity.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.drive;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+import com.google.android.gms.drive.sample.demo.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import theron_b.com.visitetablette.tool.FileManager;
+import theron_b.com.visitetablette.tool.ZipManager;
+
+/**
+ * Query file with given title and download it
+ */
+public class HomeActivity extends BaseActivity {
+
+    private static final String VILLAGE_MEDIAS = "VisiteTablette";
+    private static final String ZIP_EXT = ".zip";
+    private static final String DIR_FOR_DOWNLOADS = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+    private DriveId mFileId;
+    private int bytesExpected = 1;
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.main);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mProgressBar.setMax(100);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        super.onConnected(connectionHint);
+        bytesExpected = 1;
+        mProgressBar.setProgress(0);
+        mTextView.setText(mTextView.getText() + "\n" + "La mise à jour des médias va débuter.");
+        Query query = new Query.Builder()
+                .addFilter(Filters.contains(SearchableField.TITLE, VILLAGE_MEDIAS))
+                .build();
+        Drive.DriveApi.query(getGoogleApiClient(), query)
+                .setResultCallback(metadataCallback);
+    }
+
+    final private ResultCallback<DriveApi.MetadataBufferResult> metadataCallback =
+            new ResultCallback<DriveApi.MetadataBufferResult>() {
+                @Override
+                public void onResult(DriveApi.MetadataBufferResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        mTextView.setText(mTextView.getText() + "\n" + "Erreur lors de la récupération du résultat.");
+                        return;
+                    }
+                    bytesExpected = (int) result.getMetadataBuffer().get(0).getFileSize();
+                    mFileId = result.getMetadataBuffer().get(0).getDriveId();
+                    DriveFile file = mFileId.asDriveFile();
+                    mTextView.setText(mTextView.getText() + "\n" + "Téléchargement et mise à jour sur la tablette en cours en cours ...");
+                    new UpdateMediaAsyncTask(HomeActivity.this).execute(file);
+                }
+            };
+
+    public class UpdateMediaAsyncTask extends ApiClientAsyncTask<DriveFile, Void, Boolean> {
+
+        public UpdateMediaAsyncTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected Boolean doInBackgroundConnected(DriveFile... args) {
+            DriveFile file = args[0];
+
+            DriveApi.DriveContentsResult driveContentsResult = file.open(getGoogleApiClient(), DriveFile.MODE_READ_ONLY, null).await();
+            if (!driveContentsResult.getStatus().isSuccess()) {
+                return false;
+            }
+
+            DriveContents driveContents = driveContentsResult.getDriveContents();
+            InputStream in = driveContents.getInputStream();
+
+            try {
+                File fileToDownload = new File(DIR_FOR_DOWNLOADS + VILLAGE_MEDIAS + ZIP_EXT);
+                OutputStream out = new FileOutputStream(fileToDownload);
+                byte[] buf = new byte[1024];
+                int len;
+                int bytesDownloaded = 0;
+                while((len=in.read(buf))>0){
+                    // Update progress dialog with the latest progress.
+                    bytesDownloaded += len;
+                    int progress = (int)(bytesDownloaded*100/bytesExpected);
+                    mProgressBar.setProgress(progress);
+                    // Write the file
+                    out.write(buf,0,len);
+                }
+                out.close();
+                in.close();
+
+                // Delete folder, unzip new zip, delete zip
+                if(FileManager.Exist(fileToDownload)) {
+                    FileManager.Delete(DIR_FOR_DOWNLOADS + VILLAGE_MEDIAS);
+                    ZipManager.unZip(DIR_FOR_DOWNLOADS + VILLAGE_MEDIAS + ZIP_EXT, DIR_FOR_DOWNLOADS);
+                    FileManager.Delete(DIR_FOR_DOWNLOADS + VILLAGE_MEDIAS + ZIP_EXT);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (!result) {
+                mTextView.setText(mTextView.getText() + "\n" + "Erreur lors du téléchargement.");
+                return;
+            }
+            showMessage("Mise jour des médias réussie !");
+            finish();
+        }
+    }
+}
